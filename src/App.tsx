dead11f2af91cc342cb72ed0036d5eca0250ca60
Jesus Peralta <jesus_web_master@hotmail.com>
2025-05-09 18:25:01 -0600

    agregando base de datos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import MenuItems from "./components/MenuItem"
+import { menuItems } from "./data/db"
+
 
 function App() {
 
@@ -6,6 +9,24 @@ function App() {
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+
+          <div className="space-y-3 mt-10">
+          {menuItems.map(item => (
+            <MenuItems
+              key={item.id}
+              item={item}
+            />
+          ))}
+          </div>
+        </div>
+        <div>
+          <h2 className="">Consumo</h2>
+        </div>
+      </main>
       
     </>
   )
